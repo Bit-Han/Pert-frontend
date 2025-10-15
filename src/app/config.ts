ea6a2708b94482a1151aa5d7d64662b8653ab52e
HyperCode <72990129+Bit-Han@users.noreply.github.com>
2025-10-15 03:05:26 +0100

    changes to url

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,7 @@
-const API_BASE_URL =
-	process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "");
+const API_BASE_URL = (process.env.NEXT_PUBLIC_API_BASE_URL || "").replace(
+	/\/+$/,
+	""
+);
 export interface Task {
 	id: string;
 	optimistic: number;
