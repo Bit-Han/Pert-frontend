8f0073adc50e2059ee5633ee61ebc10ac0efec77
HyperCode <72990129+Bit-Han@users.noreply.github.com>
2025-10-15 03:22:10 +0100

    change

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || `https://pert-backend-1.onrender.com`
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || `https://pert-backend-1.onrender.com/`
 export interface Task {
 	id: string;
 	optimistic: number;
